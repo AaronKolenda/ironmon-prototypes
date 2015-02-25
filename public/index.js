8cb1590a67fd1e3d2c683c6eb5f2c1dfcce3da1d
AaronKolenda <kolenda.a@gmail.com>
2015-02-25 14:14:46 -0500

    adding player info to the page

--- public/index.js
+++ public/index.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
+
+	getTemplates();
+
 	$("#createOne").click(function(){
 		var nameOne = $("#nameOne").val();
 		var typeOne	= $("#typeOne").val();
 
 		var playerOne = new Ironmon(nameOne, typeOne);
 		console.log(playerOne);
+
+		var playerOneString = templates.playerOneInfo(playerOne);
+      	$("#leftPlayer").prepend(playerOneString);
+
 	});
 
 	$("#createTwo").click(function(){
@@ -13,6 +20,21 @@ $(document).ready(function(){
 
 		var playerTwo = new Ironmon(nameTwo, typeTwo);		
 		console.log(playerTwo);
+
+		var playerTwoString = templates.playerTwoInfo(playerTwo);
+      	$("#rightPlayer").prepend(playerTwoString);
 	});
 
 });
+
+var templates = {};
+
+var getTemplates = function(){
+
+  var playerOneTemplateString = $("#playerOne-template").text()
+  templates.playerOneInfo = Handlebars.compile(playerOneTemplateString);
+
+  var playerTwoTemplateString = $("#playerTwo-template").text()
+  templates.playerTwoInfo = Handlebars.compile(playerTwoTemplateString);
+
+  };
